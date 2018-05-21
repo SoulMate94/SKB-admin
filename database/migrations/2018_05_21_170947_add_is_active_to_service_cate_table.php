861377178e7665303eb165f6be0ebe764a1ffc5c
SoulMate94 <code0809@163.com>
2018-05-21 17:11:10 +0800

    新增字段迁移文件

---
+++ database/migrations/2018_05_21_170947_add_is_active_to_service_cate_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIsActiveToServiceCateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('skb_service_cate', function (Blueprint $table) {
+            $table->tinyInteger('is_active')->default(1)->comment('是否激活,1表示激活');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('skb_service_cate', function (Blueprint $table) {
+            //
+        });
+    }
+}
